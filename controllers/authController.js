226b28019e151de5817ff9456749730142b21245
Yerassyl Jetibay <142226512+iloveeroha@users.noreply.github.com>
2026-02-12 07:57:43 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,11 +41,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Wrong password" });
     }
 
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
-    );
+const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
     res.json({ token });
   } catch (err) {
