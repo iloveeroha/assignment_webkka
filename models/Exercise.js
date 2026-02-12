b31091cc32729686aedd73588682802114bbd385
Yerassyl Jetibay <142226512+iloveeroha@users.noreply.github.com>
2026-02-12 08:01:31 +0500

    Delete models directory

--- models/Exercise.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const exerciseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  muscle: String,
-  equipment: String
-});
-
-module.exports = mongoose.model("Exercise", exerciseSchema);
